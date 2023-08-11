7fbc72375cbaedc0bd99a5729fc06627125cfa02
Tran Tai <trananhtai577@gmail.com>
2023-08-12 02:44:49 +0700

    Update fifo_module.v
    Aug 12, 2023 version

--- fifo_module.v
+++ fifo_module.v
@@ -5,9 +5,14 @@ module fifo_module
     #(
         parameter DEPTH             = 32,
         parameter WIDTH             = 8,
+        parameter SLEEP_MODE        = 0,
+        parameter LIMIT_COUNTER     = DEPTH,
+        // Do not configuration below parameter 
         parameter COUNTER_WIDTH    = $clog2(DEPTH + 1),
         parameter DEPTH_ALIGN      = DEPTH + 1
      )(
+    // Clock for synchronous fifo
+    input clk,
     // Data
     input [WIDTH - 1:0]         data_bus_in,
     output [WIDTH - 1:0]        data_bus_out,
@@ -18,7 +23,9 @@ module fifo_module
     output              full,
     output              empty,
     // Option feature
-    output  [COUNTER_WIDTH - 1: 0] counter_elem,
+    output reach_limit,
+    // Enable pin
+    input enable,
     // Reset 
     input               rst_n
     );
@@ -30,21 +37,31 @@ module fifo_module
     reg [COUNTER_WIDTH - 1:0] front_queue;
     reg [COUNTER_WIDTH - 1:0] rear_queue;
     reg [WIDTH - 1:0] queue [0: DEPTH_ALIGN - 1];
+    wire read_ins_sleepmode;
+    wire write_ins_sleepmode;
     
+    if(SLEEP_MODE) begin
+        assign read_ins_sleepmode = (enable) ? read_ins : 1'b0;
+        assign write_ins_sleepmode = (enable) ? write_ins : 1'b0;
+    end
+    else begin
+        assign read_ins_sleepmode = read_ins;
+        assign write_ins_sleepmode = write_ins;
+    end
     //Data 
     assign data_bus_out = queue[front_queue];
     // State 
-    assign full = (counter_elem == DEPTH);
+    assign full = (rear_queue + 1 == front_queue);
     assign empty = (rear_queue == front_queue);
-    assign counter_elem = rear_queue - front_queue;
+    assign reach_limit = ((rear_queue - front_queue) >= LIMIT_COUNTER);
     // Write instruction
     always @(posedge write_ins, negedge rst_n) begin
         if(!rst_n) begin
             rear_queue <= init_index_rear;
-//            for(i = 0; i < capacity; i = i + 1) begin
-//                queue[i] <= init_buffer;
-//            end
-//            queue[capacity - 1:0] <= 0;
+    //            for(i = 0; i < capacity; i = i + 1) begin
+    //                queue[i] <= init_buffer;
+    //            end
+    //            queue[capacity - 1:0] <= 0;
         end
         else begin
             if(!full) begin
@@ -62,7 +79,6 @@ module fifo_module
             if(!empty) begin
                 front_queue <= front_queue + 1;
             end
-        end
+        end 
     end
-    
 endmodule
