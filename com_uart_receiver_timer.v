ed354e9d2134a80a5807bd6087279d0ed68000e3
Tran Tai <trananhtai577@gmail.com>
2023-08-15 03:16:04 +0700

    Update com_uart_receiver_timer.v
    Completed version

--- com_uart_receiver_timer.v
+++ com_uart_receiver_timer.v
@@ -1,13 +1,19 @@
 module com_uart_receiver_timer
-    #(
-        parameter DEVICE_CLOCK_DIV4800 = 21,  // Internal clock / 9600
-        parameter BD4800_ENCODE = 0,
-        parameter BD9600_ENCODE = 1,
-        parameter BD19200_ENCODE = 2,
-        parameter BD38400_ENCODE = 3,
+    #(  parameter CLOCK_DIVIDER =           51,     // <value> = ceil(Internal clock / (256 * 2))
+        // Unique clock use for reducing error percent (increase accuracy)
+        parameter CLOCK_DIVIDER_UNIQUE_1 =  542,    // <value> = ceil(Internal clock / (<BAUDRATE_SPEED> * 2))  (115200)
+        parameter CLOCK_DIVIDER_UNIQUE_2 =  6511,   // <value> = ceil(Internal clock / (<BAUDRATE_SPEED> * 2))  (9600)
+        parameter BD4800_ENCODE =           0,
+        parameter BD9600_ENCODE =           1,
+        parameter BD19200_ENCODE =          2,
+        parameter BD38400_ENCODE =          3,   
+        parameter BD_UNIQUE_1_ENCODE =      4,      // Unique Baudrate 
+        parameter BD_UNIQUE_2_ENCODE  =     5,      // Unique Baudrate 
         
-        parameter BAUDRATE_SEL_WIDTH = $clog2(BD38400_ENCODE),  // Max encode value
-        localparam FIRST_COUNTER_WIDTH = $clog2(DEVICE_CLOCK_DIV4800)
+        parameter BAUDRATE_SEL_WIDTH = $clog2(BD_UNIQUE_2_ENCODE + 1),  // Max encode value
+        parameter UNIQUE_1_COUNTER_WIDTH = $clog2(CLOCK_DIVIDER_UNIQUE_1 + 1), 
+        parameter UNIQUE_2_COUNTER_WIDTH = $clog2(CLOCK_DIVIDER_UNIQUE_2 + 1),  
+        localparam FIRST_COUNTER_WIDTH = $clog2(CLOCK_DIVIDER)
     )
     (
     input clk,                  // 125Mhz (internal clk)
@@ -26,7 +32,6 @@ module com_uart_receiver_timer
 //    localparam divider = DEVICE_CLOCK_DIV4800; 
     // Baudrate clock (prescaler)
 	 
-    wire rst_n_com;
     wire read_en;
     reg read_en_start;
     reg read_en_stop;
@@ -36,7 +41,6 @@ module com_uart_receiver_timer
     
     assign baudrate_div41_clk = (read_en) ? baudrate_div41 : clk;
     assign read_en = ~(read_en_start ^ read_en_stop);      
-    assign rst_n_com = rst_n & (!stop_cond);
 	 
     // Controller of "start_state" and "stop_state"  
     always @(posedge stop_cond, negedge rst_n) begin
@@ -50,18 +54,25 @@ module com_uart_receiver_timer
         end
     end
     // Controller of Divider
-    always @(posedge clk, negedge rst_n) begin
+    
+    
+    wire normal_mode_clk;
+    wire normol_mode_en = ( baudrate_sel == BD4800_ENCODE   | baudrate_sel == BD9600_ENCODE |
+                            baudrate_sel == BD19200_ENCODE  | baudrate_sel == BD38400_ENCODE);
+    
+    assign normal_mode_clk = (normol_mode_en) ? clk : 1'b0;
+    always @(posedge normal_mode_clk, negedge rst_n) begin
         if(!rst_n) begin
             baudrate_div41 <= 0; 
-            counter_div41 <= (DEVICE_CLOCK_DIV4800 - 1);    
+            counter_div41 <= (CLOCK_DIVIDER - 1);    
         end
         else begin 
             if(!read_en) begin
                 baudrate_div41 <= 0; 
-                counter_div41 <= (DEVICE_CLOCK_DIV4800 - 1); 
+                counter_div41 <= (CLOCK_DIVIDER - 1); 
             end
             else begin
-                if(counter_div41 == (DEVICE_CLOCK_DIV4800 - 1)) begin 
+                if(counter_div41 == (CLOCK_DIVIDER - 1)) begin 
                     baudrate_div41 <= ~baudrate_div41;
                     counter_div41 <= 0;
                 end
@@ -104,9 +115,66 @@ module com_uart_receiver_timer
             end
         end
     end
-    assign baudrate_clk =   (baudrate_sel == BD9600_ENCODE) ? baudrate_div41_div128 : 
-                            (baudrate_sel == BD19200_ENCODE) ? baudrate_div41_div64 : 
-                            (baudrate_sel == BD38400_ENCODE) ? baudrate_div41_div32 : baudrate_div41_div16;
+    
+    // Unique_1 clock mode 
+    wire unique_1_mode_clk;
+    reg baudrate_unique_1;
+    reg [UNIQUE_1_COUNTER_WIDTH - 1:0] counter_unique_1;
+    
+    assign unique_1_mode_clk = (baudrate_sel == BD_UNIQUE_1_ENCODE) ? clk : 1'b0;
+    
+    always @(posedge unique_1_mode_clk, negedge rst_n) begin
+        if(!rst_n) begin
+            counter_unique_1 <= (CLOCK_DIVIDER_UNIQUE_1 - 1);
+            baudrate_unique_1 <= 0; 
+        end
+        else begin
+            if(!read_en) begin
+                counter_unique_1 <= (CLOCK_DIVIDER_UNIQUE_1 - 1);
+                baudrate_unique_1 <= 0; 
+            end
+            else begin
+                if(counter_unique_1 == (CLOCK_DIVIDER_UNIQUE_1 - 1)) begin
+                    counter_unique_1 <= 0;
+                    baudrate_unique_1 <= ~baudrate_unique_1;
+                end
+                else counter_unique_1 <= counter_unique_1 + 1;
+            end
+        end
+    end
+    
+    // Unique_2 clock mode 
+    wire unique_2_mode_clk;
+    reg baudrate_unique_2;
+    reg [UNIQUE_2_COUNTER_WIDTH - 1:0] counter_unique_2;
+    
+    assign unique_2_mode_clk = (baudrate_sel == BD_UNIQUE_2_ENCODE) ? clk : 1'b0;
+    
+    always @(posedge unique_2_mode_clk, negedge rst_n) begin
+        if(!rst_n) begin
+            counter_unique_2 <= (CLOCK_DIVIDER_UNIQUE_2 - 1);
+            baudrate_unique_2 <= 0; 
+        end
+        else begin
+            if(!read_en) begin
+                counter_unique_2 <= (CLOCK_DIVIDER_UNIQUE_2 - 1);
+                baudrate_unique_2 <= 0; 
+            end
+            else begin
+                if(counter_unique_2 == (CLOCK_DIVIDER_UNIQUE_2 - 1)) begin
+                    counter_unique_2 <= 0;
+                    baudrate_unique_2 <= ~baudrate_unique_2;
+                end
+                else counter_unique_2 <= counter_unique_2 + 1;
+            end
+        end
+    end
+    ////////////////////////////
+    assign baudrate_clk =   (baudrate_sel == BD4800_ENCODE) ? baudrate_div41_div128 : 
+                            (baudrate_sel == BD9600_ENCODE) ? baudrate_div41_div64 : 
+                            (baudrate_sel == BD19200_ENCODE) ? baudrate_div41_div32 : 
+                            (baudrate_sel == BD38400_ENCODE) ? baudrate_div41_div16 : 
+                            (baudrate_sel == BD_UNIQUE_1_ENCODE) ? baudrate_unique_1 : baudrate_unique_2;
 
 //    Debug area /////////////////////////////////
 //    assign baudrate_clk = baudrate_div41_div128;    
