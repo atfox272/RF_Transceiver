249bb3ac18a7244bbe2b022d6f1dd390f8e30b95
Tran Tai <trananhtai577@gmail.com>
2023-08-26 03:55:32 +0700

    Update RX_transceiver_general_tb.v

--- RX_transceiver_general_tb.v
+++ RX_transceiver_general_tb.v
@@ -20,11 +20,12 @@ module RF_transceiver_general_tb;
     // Common            
     reg M0;
     reg M1;
+    wire [1:0] mode_controller_wire;
     reg device_clk;
     wire internal_clk;
     reg rst_n;
     wire AUX;                               //             BD_9600      BD_115200
-    wire [7:0] all_common_config = (M0 == 1 & M1 == 1) ? 8'b10100011 : 8'b10000011;
+    wire [7:0] all_common_config = (mode_controller_wire == 3) ? 8'b10100011 : 8'b10000011;
     wire [7:0] TX_node_external_config = 8'b10000011;
     // UART mcu
     reg [7:0] data_in_mcu_external;
@@ -55,6 +56,7 @@ module RF_transceiver_general_tb;
     wire [DATA_WIDTH - 1:0] data_in_uart_node_wire;
     
     wire [3:0] state_module_wire;
+    
     
     prescaler_module#(
                     .IDLE_CLK(1'b0),
@@ -132,6 +134,7 @@ module RF_transceiver_general_tb;
 //                    ,.TX_use_node_wire(TX_use_node_wire)
 //                    ,.data_in_uart_node_wire(data_in_uart_node_wire)
 //                    ,.internal_clk_wire(internal_clk)
+                    ,.mode_controller_wire(mode_controller_wire)
                     );
     initial begin
         device_clk <= 0;
@@ -148,122 +151,6 @@ module RF_transceiver_general_tb;
         #2 rst_n <= 0;
         #8 rst_n <= 1;
     end
-    // For wireless-transmitter
-//    initial begin
-//        #12;
-        
-//        data_in_mcu_external <= 8'h27;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h11;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h22;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h33;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h44;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h55;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h66;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h77;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-//    end
-    // Test wireless-receiver
-//    initial begin
-//        #12;
-        
-//        data_in_node_external <= 8'hFF;
-//        #1 TX_use_node_external <= 1;
-//        #1 TX_use_node_external <= 0;
-        
-//        data_in_node_external <= 8'h11;
-//        #1 TX_use_node_external <= 1;
-//        #1 TX_use_node_external <= 0;
-        
-//        data_in_node_external <= 8'h22;
-//        #1 TX_use_node_external <= 1;
-//        #1 TX_use_node_external <= 0;
-        
-//        data_in_node_external <= 8'h33;
-//        #1 TX_use_node_external <= 1;
-//        #1 TX_use_node_external <= 0;
-        
-//        data_in_node_external <= 8'h44;
-//        #1 TX_use_node_external <= 1;
-//        #1 TX_use_node_external <= 0;
-        
-//        data_in_node_external <= 8'h55;
-//        #1 TX_use_node_external <= 1;
-//        #1 TX_use_node_external <= 0;
-        
-//        data_in_node_external <= 8'h00;
-//        #1 TX_use_node_external <= 1;
-//        #1 TX_use_node_external <= 0;
-//    end
-    initial begin
-        #12;
-        
-        
-//        data_in_mcu_external <= 8'h27;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h11;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h22;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h33;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h44;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h55;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h66;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        data_in_mcu_external <= 8'h77;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-//        #1000;
-//        M0 <= 1;
-//        M1 <= 1;
-        
-        
-//        data_in_mcu_external <= 8'h77;
-//        #1 TX_use_mcu_external <= 1;
-//        #1 TX_use_mcu_external <= 0;
-        
-        
-        
-    end
     
     initial begin
         forever #1 device_clk <= ~device_clk;
@@ -277,137 +164,6 @@ module RF_transceiver_general_tb;
     localparam CONFIG_STATE = 4;
     localparam READY_STATE = 6;
     localparam END_STATE = 7;
-    
-    
-//    initial begin
-//        state_counter <= IDLE_STATE;
-//        #10000;
-//        #100000;
-//        M0 <= 0;
-//        M1 <= 0;
-//        #1000000;
-       
-//        M1 <= 1;
-//        M0 <= 1;
-//        #10;
-//        // Ask transceiver
-//                data_in_mcu_external <= 8'hC0;  // HEAD
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'hCD;  // ADDH
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                    
-//                data_in_mcu_external <= 8'hAB;  // ADDL
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h3D;  // SPED
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h17;  // CHAN
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'hC4;  // OPTION 
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-                
-//               #900000;
-//               M1 <= 0;
-//               M0 <= 0;
-               
-//               #450000;
-//               M1 <= 1;
-//               M0 <= 1;
-               
-//               #100000;
-//                data_in_mcu_external <= 8'hC1;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'hC1;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'hC1;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-                
-//                // Send testing
-                
-//                #1000000;
-               
-//                M1 <= 0;
-//                M0 <= 0;
-//                #100;
-//                data_in_mcu_external <= 8'h27;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h11;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h22;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h33;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h44;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h55;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h66;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h77;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                // receive testing
-//                 #1000000;
-//                 #1000;
-//                data_in_node_external <= 8'hFF;
-//                #1 TX_use_node_external <= 1;
-//                #1 TX_use_node_external <= 0;
-                
-//                data_in_node_external <= 8'h11;
-//                #1 TX_use_node_external <= 1;
-//                #1 TX_use_node_external <= 0;
-                
-//                data_in_node_external <= 8'h22;
-//                #1 TX_use_node_external <= 1;
-//                #1 TX_use_node_external <= 0;
-                
-//                data_in_node_external <= 8'h33;
-//                #1 TX_use_node_external <= 1;
-//                #1 TX_use_node_external <= 0;
-                
-//                data_in_node_external <= 8'h44;
-//                #1 TX_use_node_external <= 1;
-//                #1 TX_use_node_external <= 0;
-                
-//                data_in_node_external <= 8'h55;
-//                #1 TX_use_node_external <= 1;
-//                #1 TX_use_node_external <= 0;
-                
-//                data_in_node_external <= 8'h00;
-//                #1 TX_use_node_external <= 1;
-//                #1 TX_use_node_external <= 0;
-//    end
     
     // Prescaler case
     initial begin
@@ -470,47 +226,28 @@ module RF_transceiver_general_tb;
                 
                 // Send testing
                 
-                #10000000;
+                #1500000;
                
                 M1 <= 0;
                 M0 <= 0;
+                
+                // wireless-trans (512bytes-packet)
+                #7500000;
                 #1000;
                 for(integer i = 0; i < 512; i = i + 1) begin
                     data_in_mcu_external <= i;
                     #1 TX_use_mcu_external <= 1;
                     #1 TX_use_mcu_external <= 0;
                 end
-//                data_in_mcu_external <= 8'h27;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
                 
-//                data_in_mcu_external <= 8'h11;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h22;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h33;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h44;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h55;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h66;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
-                
-//                data_in_mcu_external <= 8'h77;
-//                #1 TX_use_mcu_external <= 1;
-//                #1 TX_use_mcu_external <= 0;
+                // wireless-trans (less than 58bytes-packet)
+                 #30000000;
+                 #10000;
+                for(integer i = 0; i < 50; i = i + 1) begin
+                    data_in_mcu_external <= 511 - i;
+                    #1 TX_use_mcu_external <= 1;
+                    #1 TX_use_mcu_external <= 0;
+                end
                 
                 // receive testing
                  #30000000;
@@ -521,6 +258,8 @@ module RF_transceiver_general_tb;
                     #1 TX_use_node_external <= 1;
                     #1 TX_use_node_external <= 0;
                 end
+                
+                
 //                data_in_node_external <= 8'hFF;
 //                #1 TX_use_node_external <= 1;
 //                #1 TX_use_node_external <= 0;
