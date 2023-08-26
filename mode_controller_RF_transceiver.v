f84ee25f3508a8bd3806b13118605a4558c7fee6
Tran Tai <trananhtai577@gmail.com>
2023-08-27 05:46:25 +0700

    Update mode_controller_RF_transceiver.v
    Update: AUX is LOW when the module switches mode from stand-by mode to others mode

--- mode_controller_RF_transceiver.v
+++ mode_controller_RF_transceiver.v
@@ -1,7 +1,8 @@
 module mode_controller_RF_transceiver
     #(
-        parameter DEFAULT_MODE = 2'd3,
-        parameter END_MODE_SWITCH = 10000
+        parameter DEFAULT_MODE      = 2'd3,
+        parameter END_MODE_SWITCH   = 10000,
+        parameter AUX_POWER_ON      = 1'b1      // State of AUX in power-on state (In Real module, Its HIGH)
     )
     (
     input   wire    internal_clk,
@@ -43,15 +44,16 @@ module mode_controller_RF_transceiver
         if(!rst_n) begin
             M1_sync <= DEFAULT_MODE[1];
             M0_sync <= DEFAULT_MODE[0];
-            state_counter_mode_ctrl <= INIT_STATE;
+            state_counter_mode_ctrl <= IDLE_STATE;
             // Repair for self-checking when module is reset
-            AUX_mode_ctrl <= 0;
+            // In real module, AUX of E32 is not LOW when module is opening
+            AUX_mode_ctrl <= AUX_POWER_ON;
         end
         else begin
             case(state_counter_mode_ctrl) 
                 INIT_STATE: begin
                      // AUX controller
-                     AUX_mode_ctrl <= 0;
+                     AUX_mode_ctrl <= AUX_POWER_ON;
                      if(end_mode_switch) begin
                         state_counter_mode_ctrl <= IDLE_STATE;
                         AUX_mode_ctrl <= 1;
@@ -61,22 +63,22 @@ module mode_controller_RF_transceiver
                     // Mode is switching and Module is free now
                     if(AUX_state_ctrl & UART_mcu_complete) begin
                         // Recommand: "e mode changes from stand-by mode to others, the module will reset its parameters, during which the AUX keeps low level and then outputs high level after reset completed"
-//                        if((cur_mode == MODE_3) & (new_mode != MODE_3)) begin
-//                            state_counter_mode_ctrl <= MODE_SWITCH_STATE;
-//                            // AUX controller
-//                            AUX_mode_ctrl <= 0;
-//                            M1_sync <= M1;
-//                            M0_sync <= M0;
-//                        end
+                        if((cur_mode == MODE_3) & (new_mode != MODE_3)) begin
+                            state_counter_mode_ctrl <= MODE_SWITCH_STATE;
+                            // AUX controller
+                            AUX_mode_ctrl <= 0;
+                            M1_sync <= M1;
+                            M0_sync <= M0;
+                        end
 //                        // Mode swithcing when
-//                        else if(~(new_mode == cur_mode)) begin
-//                            M1_sync <= M1;
-//                            M0_sync <= M0;    
-//                        end
-                        if(~(new_mode == cur_mode)) begin
+                        else if(~(new_mode == cur_mode)) begin
                             M1_sync <= M1;
                             M0_sync <= M0;    
                         end
+//                        if(~(new_mode == cur_mode)) begin
+//                            M1_sync <= M1;
+//                            M0_sync <= M0;    
+//                        end
                     end
                 end
                 MODE_SWITCH_STATE: begin        // Switching from Stand-by mode (Mode 3) to others mode
