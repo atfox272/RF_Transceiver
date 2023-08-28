a65583253425048002f262a1778087edfb3ef41d
Tran Tai <trananhtai577@gmail.com>
2023-08-29 00:25:31 +0700

    Update self-checking in INIT_STATE

--- mode_controller_RF_transceiver.v
+++ mode_controller_RF_transceiver.v
@@ -2,6 +2,7 @@ module mode_controller_RF_transceiver
     #(
         parameter DEFAULT_MODE      = 2'd3,
         parameter END_MODE_SWITCH   = 10000,
+        parameter END_POWER_ON_CHECK= 750000,   // Asume: 500ms
         parameter AUX_POWER_ON      = 1'b0      // State of AUX in power-on state (In Real module, Its HIGH)
     )
     (
@@ -10,6 +11,7 @@ module mode_controller_RF_transceiver
     input   wire    M1,
     input   wire    AUX_state_ctrl,
     output  reg     AUX_mode_ctrl,
+    output  reg     AUX_power_on_ctrl,
     input   wire    AUX_uart_ctrl,
     input   wire    UART_mcu_complete,
     
@@ -19,7 +21,8 @@ module mode_controller_RF_transceiver
     input   wire    rst_n
     );
     reg [1:0] state_counter_mode_ctrl;
-    wire end_mode_switch;
+    wire stop_waiting_mode_switching_signal;
+    wire stop_waiting_power_on_signal;
     wire [1:0] cur_mode = {M1_sync, M0_sync};
     wire [1:0] new_mode = {M1, M0};
     // FSM Encoder
@@ -31,14 +34,27 @@ module mode_controller_RF_transceiver
     localparam MODE_1 = 1;
     localparam MODE_2 = 2;
     localparam MODE_3 = 3;
+    
     waiting_module #(
                     .END_COUNTER(END_MODE_SWITCH),
                     .WAITING_TYPE(0),
                     .LEVEL_PULSE(0)
-                    )waiting_RX_packet(
+                    )waiting_mode_switching(
                     .clk(internal_clk),
                     .start_counting(AUX_mode_ctrl),
-                    .reach_limit(end_mode_switch),
+                    .reach_limit(stop_waiting_mode_switching_signal),
+                    .rst_n(rst_n)
+                    );
+                        
+                 
+    waiting_module #(
+                    .END_COUNTER(END_POWER_ON_CHECK),
+                    .WAITING_TYPE(0),
+                    .LEVEL_PULSE(0)
+                    )waiting_power_on(
+                    .clk(internal_clk),
+                    .start_counting(AUX_power_on_ctrl),
+                    .reach_limit(stop_waiting_power_on_signal),
                     .rst_n(rst_n)
                     );
     always @(posedge internal_clk, negedge rst_n) begin
@@ -47,22 +63,22 @@ module mode_controller_RF_transceiver
             M0_sync <= DEFAULT_MODE[0];
             state_counter_mode_ctrl <= INIT_STATE;
             // Repair for self-checking when module is reset
-            // In real module, AUX of E32 is not LOW when module is opening
-            AUX_mode_ctrl <= AUX_POWER_ON;
+            AUX_mode_ctrl <= 1'b1;
+            AUX_power_on_ctrl <= AUX_POWER_ON;
         end
         else begin
             case(state_counter_mode_ctrl) 
                 INIT_STATE: begin
                      // AUX controller
-                     AUX_mode_ctrl <= AUX_POWER_ON;
-                     if(end_mode_switch) begin
+                     AUX_power_on_ctrl <= AUX_POWER_ON;
+                     if(stop_waiting_power_on_signal) begin
                         state_counter_mode_ctrl <= IDLE_STATE;
-                        AUX_mode_ctrl <= 1;
+                        AUX_power_on_ctrl <= 1;
                     end
                 end
                 IDLE_STATE: begin
                     // Mode is switching and Module is free now
-                    if(AUX_state_ctrl & AUX_uart_ctrl) begin
+                    if(AUX_state_ctrl & AUX_uart_ctrl & AUX_power_on_ctrl) begin
                         // Recommand: "e mode changes from stand-by mode to others, the module will reset its parameters, during which the AUX keeps low level and then outputs high level after reset completed"
                         if((cur_mode == MODE_3) & (new_mode != MODE_3)) begin
                             state_counter_mode_ctrl <= MODE_SWITCH_STATE;
@@ -83,7 +99,7 @@ module mode_controller_RF_transceiver
                     end
                 end
                 MODE_SWITCH_STATE: begin        // Switching from Stand-by mode (Mode 3) to others mode
-                    if(end_mode_switch) begin
+                    if(stop_waiting_mode_switching_signal) begin
                         state_counter_mode_ctrl <= IDLE_STATE;
                         AUX_mode_ctrl <= 1;
                     end
